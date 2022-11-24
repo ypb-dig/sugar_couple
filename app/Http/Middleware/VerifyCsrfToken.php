dc40a3d14e11658ce4a8cec07ffb653cdcf29471
Rodrigo Costa <rodrigocosta47@outlook.com>
2022-11-23 23:07:13 -0300

    Adicionei uma nova rota sem auth

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        "https://sandbox.pagseguro.uol.com.br/"
+        "https://sandbox.pagseguro.uol.com.br"
     ];
 }
