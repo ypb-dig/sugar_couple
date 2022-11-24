360dc500dcc8c6e3c5983dc8a8da380dc0c8df7a
Rodrigo Costa <rodrigocosta47@outlook.com>
2022-11-24 10:24:18 -0300

    Adiccionei a rota aos verify

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::post('notificationstatus/{notificationType?}/{notificationCode?}', [
+Route::post('notificationstatus/', [
     'as' => 'pagseguro.notification',
     'uses' => 'NotificationApiController@notificationStatus'
 ]);
