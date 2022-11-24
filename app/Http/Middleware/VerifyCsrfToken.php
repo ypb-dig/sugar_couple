7ce728eb4958d76d04c989c3f676354f761d5939
Rodrigo Costa <rodrigocosta47@outlook.com>
2022-11-23 22:53:15 -0300

    Atualizei

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        'notifiation-status*'
+        "https://sandbox.pagseguro.uol.com.br/"
     ];
 }
