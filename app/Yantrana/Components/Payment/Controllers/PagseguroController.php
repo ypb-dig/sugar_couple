360e670b385a2183065b36168d3fbcb0745022cd
Rodrigo Costa <rodrigocosta47@outlook.com>
2022-11-28 11:00:43 -0300

    Pre order returna uma string com reff mais o id

--- app/Yantrana/Components/Payment/Controllers/PagseguroController.php
+++ app/Yantrana/Components/Payment/Controllers/PagseguroController.php
@@ -73,7 +73,7 @@ class PagseguroController extends BaseController
             $request->get('itemPrice1')
         );
 
-        $payment->setReference("reff-1");
+        $payment->setReference($preOrderId);
 
         $payment->setCurrency("BRL");
 
